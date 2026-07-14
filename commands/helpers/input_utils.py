e97b0804c37f84631d6011a143d5f227403cf143
Sameer Patwardhan <sampatwardhan1112@gmail.com>
2026-07-14 17:51:51 +0530

    Add get_selected_name and get_input functions

--- commands/helpers/input_utils.py
+++ commands/helpers/input_utils.py
@@ -35,11 +35,20 @@ def get_selected_item(inputs, input_id):
         return ""
 
 
+def get_selected_name(inputs, input_id):
+    """Return the selected item exactly as displayed."""
+
+    try:
+        return inputs.itemById(input_id).selectedItem.name
+    except:
+        return ""
+
+
 def set_visibility(inputs, input_id, visible):
     """Set visibility of an input."""
     try:
         input_obj = inputs.itemById(input_id)
-        if input_obj:
+        if input_obj is not None:
             input_obj.isVisible = visible
     except:
         pass
@@ -49,7 +58,24 @@ def set_numeric_value(inputs, input_id, value):
     """Set numeric value of an input."""
     try:
         input_obj = inputs.itemById(input_id)
-        if input_obj:
+        if input_obj is not None:
             input_obj.value = value
+    except:
+        pass
+
+def get_input(inputs, input_id):
+    """Return an input object by ID."""
+    try:
+        return inputs.itemById(input_id)
+    except:
+        return None
+
+
+def set_text_value(inputs, input_id, text):
+    """Update the text of a TextBoxCommandInput."""
+    try:
+        input_obj = inputs.itemById(input_id)
+        if input_obj is not None:
+            input_obj.text = text
     except:
         pass
