7f0dff2d6a83d89fef1086954436fa80136c74da
Sameer Patwardhan <sampatwardhan1112@gmail.com>
2026-09-12 11:40:38 +0530

    Simplify package CMake install for generated description resources

--- exporter/cmake_generator.py
+++ exporter/cmake_generator.py
@@ -1,64 +1,23 @@
-"""
-cmake_generator.py
-
-Generates the CMakeLists.txt file for a ROS 2
-robot description package.
-"""
+"""Generate CMakeLists.txt for the exported ROS 2 package."""
 
 from .file_writer import FileWriter
 
 
 class CMakeGenerator:
-
-    def __init__(
-        self,
-        robot,
-        package_creator
-    ):
-
+    def __init__(self, robot, package_creator):
         self.robot = robot
         self.package = package_creator
-
-        self.writer = FileWriter(
-            self.package.package_directory()
-        )
-
-    # =====================================================
-    # Generate CMakeLists.txt
-    # =====================================================
+        self.writer = FileWriter(self.package.package_directory())
 
     def generate(self):
-
-        cmake = self._build_cmake()
-
-        self.writer.write_file(
-            "CMakeLists.txt",
-            cmake
-        )
-
-    # =====================================================
-    # Build CMakeLists
-    # =====================================================
+        return self.writer.write_file("CMakeLists.txt", self._build_cmake())
 
     def _build_cmake(self):
-
         package = self.robot.package_name
-
-        return f"""cmake_minimum_required(VERSION 3.8)
+        return f'''cmake_minimum_required(VERSION 3.8)
 project({package})
 
-if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  add_compile_options(-Wall -Wextra -Wpedantic)
-endif()
-
 find_package(ament_cmake REQUIRED)
-
-if(BUILD_TESTING)
-  find_package(ament_lint_auto REQUIRED)
-  set(ament_cmake_copyright_FOUND TRUE)
-  set(ament_cmake_cpplint_FOUND TRUE)
-  ament_lint_auto_find_test_dependencies()
-endif()
 
 install(
   DIRECTORY config launch urdf meshes rviz worlds
@@ -66,4 +25,4 @@ install(
 )
 
 ament_package()
-"""
+'''
