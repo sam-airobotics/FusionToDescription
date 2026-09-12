0f9e9e1c70eb2f61893af0da6a2dba0e2bc8dcab
Sameer Patwardhan <sampatwardhan1112@gmail.com>
2026-09-12 11:41:04 +0530

    Generate URDF materials from extracted Fusion colors

--- exporter/materials_xacro_generator.py
+++ exporter/materials_xacro_generator.py
@@ -1,9 +1,4 @@
-"""
-materials_xacro_generator.py
-
-Generates the materials.xacro file containing visual
-material definitions extracted from the RobotModel.
-"""
+"""Generate materials.xacro with extracted appearance colors."""
 
 from xml.sax.saxutils import quoteattr
 
@@ -11,87 +6,28 @@ from .file_writer import FileWriter
 
 
 class MaterialsXacroGenerator:
-
-    def __init__(
-        self,
-        robot,
-        package_creator,
-        config=None
-    ):
-        """
-        Initialize materials xacro generator.
-        """
-
+    def __init__(self, robot, package_creator, config=None):
         self.robot = robot
         self.package = package_creator
         self.config = config
-
-        self.writer = FileWriter(
-            self.package.package_directory()
-        )
-
-    # =====================================================
-    # Generate
-    # =====================================================
+        self.writer = FileWriter(self.package.package_directory())
 
     def generate(self):
-
-        self.writer.write_file(
-            "urdf/materials.xacro",
-            self._build_xacro()
-        )
-
-    # =====================================================
-    # Build
-    # =====================================================
+        return self.writer.write_file("urdf/materials.xacro", self._build_xacro())
 
     def _build_xacro(self):
-
-        xacro = """<?xml version="1.0"?>
-
-<robot xmlns:xacro="http://www.ros.org/wiki/xacro">
-
-    <!-- ================================================= -->
-    <!-- Material Definitions                              -->
-    <!-- ================================================= -->
-"""
-
+        xacro = '<?xml version="1.0"?>\n<robot xmlns:xacro="http://www.ros.org/wiki/xacro">\n'
         exported = set()
-
         for link in self.robot.links:
-
             material = link.material
-
-            if material is None:
+            if material is None or not material.name or material.name in exported:
                 continue
-
-            if not material.name:
-                continue
-
-            if material.name in exported:
-                continue
-
             exported.add(material.name)
-
             color = material.color
-
-            if color is None:
-                r = g = b = 0.7
-                a = 1.0
-            else:
-                r = color.r
-                g = color.g
-                b = color.b
-                a = color.a
-
-            xacro += f"""
-    <material name={quoteattr(material.name)}>
-        <color rgba="{r:.6f} {g:.6f} {b:.6f} {a:.6f}"/>
-    </material>
-"""
-
-        xacro += """
-</robot>
-"""
-
-        return xacro
+            r, g, b, a = color.r, color.g, color.b, color.a
+            xacro += (
+                f'  <material name={quoteattr(material.name)}>\n'
+                f'    <color rgba="{r:.6f} {g:.6f} {b:.6f} {a:.6f}"/>\n'
+                '  </material>\n'
+            )
+        return xacro + '</robot>\n'
