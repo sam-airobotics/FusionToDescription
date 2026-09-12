da0898bd08d81d423989053da1ef25d9f6b2d373
Sameer Patwardhan <sampatwardhan1112@gmail.com>
2026-09-12 11:42:18 +0530

    Embed ros2_control in main robot Xacro for valid root-level URDF tags

--- exporter/robot_xacro_generator.py
+++ exporter/robot_xacro_generator.py
@@ -21,13 +21,10 @@ class RobotXacroGenerator:
         renderer = URDFGenerator(self.robot, self.package, self.config)
         xacro = f'''<?xml version="1.0"?>
 <robot xmlns:xacro="http://www.ros.org/wiki/xacro" name={quoteattr(self.robot.robot_name)}>
-
   <xacro:include filename="$(find {package})/urdf/materials.xacro"/>
 '''
         if self.config and self.config.generate_gazebo:
             xacro += f'  <xacro:include filename="$(find {package})/urdf/gazebo.xacro"/>\n'
-        if self.config and self.config.generate_ros2_control:
-            xacro += f'  <xacro:include filename="$(find {package})/urdf/ros2_control.xacro"/>\n'
 
         if self._needs_base_footprint():
             xacro += '''
@@ -37,16 +34,43 @@ class RobotXacroGenerator:
     <parent link="base_footprint"/>
     <child link="base_link"/>
   </joint>
-
 '''
 
         for link in self.robot.links:
             xacro += renderer._generate_link(link, xacro=True)
-        # Keep all joints in one source of truth. This avoids the old duplicated
-        # joint definitions in joints.xacro and the main Xacro.
-        xacro += '\n  <xacro:include filename="$(find ' + package + ')/urdf/joints.xacro"/>\n'
-        xacro += '\n</robot>\n'
-        return xacro
+
+        # Generate ros2_control directly inside the main robot so all root-level
+        # URDF tags are guaranteed to live under the same <robot> element.
+        if self.config and self.config.generate_ros2_control:
+            xacro += f'''\n  <ros2_control name="GazeboSimSystem" type="system">
+    <hardware>
+      <plugin>gz_ros2_control/GazeboSimSystem</plugin>
+    </hardware>
+'''
+            for joint in self.robot.joints:
+                if joint.joint_type != "fixed":
+                    xacro += f'''    <joint name="{joint.name}">
+      <command_interface name="position"/>
+      <state_interface name="position"/>
+      <state_interface name="velocity"/>
+    </joint>
+'''
+            xacro += '''  </ros2_control>
+'''
+
+        # Keep joint definitions in one file and include that fragment inside
+        # the same robot element.
+        xacro += f'  <xacro:include filename="$(find {package})/urdf/joints.xacro"/>\n'
+
+        if self.config and self.config.generate_ros2_control:
+            xacro += '''  <gazebo>
+    <plugin filename="libgz_ros2_control-system.so" name="gz_ros2_control::GazeboSimROS2ControlPlugin">
+      <parameters>$(find ''' + package + ''')/config/controllers.yaml</parameters>
+    </plugin>
+  </gazebo>
+'''
+
+        return xacro + '</robot>\n'
 
     def _needs_base_footprint(self):
         if self.robot.get_link("base_link") is None or self.robot.get_link("base_footprint") is not None:
